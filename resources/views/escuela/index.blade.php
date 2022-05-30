91265f3fab247dc36486a9f2e42d0ff2bcd4e63c
Luis Angel Valverde <luis.valverde29344@alumnos.udg.mx>
2022-05-30 13:10:15 -0500

    Implementación de modelos y migraciones

---
+++ resources/views/escuela/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Escuela</title>
+</head>
+<body>
+  <h1>Estudiantes</h1>
+  <table>
+    <tr>
+      <th>Nombre</th>
+      <th>Código</th>
+      <th>Carrera</th>
+      <th>Créditos</th>
+      <th>Correo</th>        
+    </tr>        
+  </table>
+  <br>  
+  <a href="escuela/create">Agregar</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EstudianteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('/index', [EstudianteController::class, 'index']);
+
+Route::resource('/escuela', EstudianteController::class);
